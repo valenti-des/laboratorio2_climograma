0b1f22dae2e72454d3d44174ee228dc9b3cb14ef
valenti-des <valentindestri@gmail.com>
2026-04-08 08:20:33 -0300

    version 3 solo le falta agregar el asqui y si  es templado el miniimo  y  maximo de temperatrura y presipitacion y promedio

--- clima/clima/Program.cs
+++ clima/clima/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Security.Principal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 float [] temp = new float [12];
 
 float [] presipitacion = new float [12];
@@ -19,8 +22,23 @@ for (int i = 0; i < 12; i++)
 {
 
 
+
     Console.WriteLine("coloca la temperatura del mes " + meses[i]);
-    temp[i] = float.Parse(Console.ReadLine());
+     string temperatura = Console.ReadLine();
+
+    if (float.TryParse( temperatura , out temp[i]))
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("La temperatura tiene que ser numerica. no se admiten caracteres especiales");
+
+        i--;
+
+        continue;
+    }
+
 
 
     if (temp[i] > 57 || temp[i] < -89)
@@ -43,10 +61,29 @@ for (int i = 0; i < 12; i++)
     
 
         Console.WriteLine("coloca la presipitacio del mes " + meses[i]);
-        presipitacion[i] = float.Parse(Console.ReadLine());
+        string  presipitacio = Console.ReadLine();
 
 
-        if (presipitacion[i] > 1825 || presipitacion[i] < 0.86) 
+
+        if (float.TryParse(presipitacio, out presipitacion[i]))
+        {
+
+        }
+        else
+        {
+            Console.WriteLine("La presipitacion tiene que ser numerica. no se admiten caracteres especiales");
+
+            i--;
+
+            continue;
+        }
+
+
+
+
+
+
+    if (presipitacion[i] > 1825 || presipitacion[i] < 0.86) 
         {
         Console.WriteLine("Presipitacion fuera de lo normal. intentelo de nuevo.");
 
@@ -112,7 +149,7 @@ for (int i = 0; i < 12; i++)
 
 // comparacion  temperatura mayor y menor
 
-float temperatura_mayor = -89;
+float temperatura_mayor = -10000000;
 
 string maximo_mes= "";
 
@@ -123,7 +160,7 @@ for (int i = 0; i < 12; i++)
 
     if (temp[i] > temperatura_mayor  )
     {
-        temperatura_mayor = temp[i];
+        temperatura_mayor = temp[i];    
         maximo_mes = meses[i];
     }
 
@@ -138,7 +175,7 @@ Console.WriteLine("la  temperatura mayor   es  " + temperatura_mayor +  "  del m
 
 
 
-float temperatura_menor = 57;
+float temperatura_menor = 10000000;
 
 string menor_mes = "";
 
@@ -160,4 +197,83 @@ Console.WriteLine("la  temperatura menor   es  " + temperatura_menor + "  del me
 
 
 
-//--------------------------------------------------------------------------------------presipitcion
+
+
+// comparacion presipitacion  menor y mayor
+
+
+
+float presipitacion_mayor = -10000000;
+
+string maximop_mes = "";
+
+for (int i = 0; i < 12; i++)
+{
+
+
+
+    if (presipitacion[i] > presipitacion_mayor)
+    {
+        presipitacion_mayor = presipitacion [i];
+        maximop_mes = meses[i];
+    }
+
+
+}
+
+Console.WriteLine("la  presipitacion mayor   es  " + presipitacion_mayor + "  del mes  " + maximop_mes);
+
+
+
+
+
+
+
+float presipitacion_menor = 10000000;
+
+string menorp_mes = "";
+
+for (int i = 0; i < 12; i++)
+{
+
+
+
+    if (presipitacion[i] < presipitacion_menor)
+    {
+        presipitacion_menor = presipitacion[i];
+        menorp_mes = meses[i];
+    }
+
+
+}
+
+Console.WriteLine("la  temperatura menor   es  " + presipitacion_menor + "  del mes  " + menorp_mes);
+
+
+
+
+// promedio temp 
+float total = default;
+
+for (int i = 0; i < 12; i++)
+{
+
+
+
+     total = total + temp[i];
+
+}
+
+float promedio_temp = default;
+
+promedio_temp = total / 12;
+
+
+Console.WriteLine($"la temperatura promedio del año es {promedio_temp}");
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------presipitcion promedio
